9d3125fb008614f5125787c236e5f7051bab884f
Alex <sandylily66@hotmail.com>
2017-01-20 23:24:13 +0000

    Updated files and added scene

--- Assets/Code/Spawner.cs
+++ Assets/Code/Spawner.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Spawner : MonoBehaviour {
+public class Spawner : MonoBehaviour
+{
 
+    public GameObject[] prefabs;
+    public float delay = 2.0f;
+    public bool active = true;
+        
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        StartCoroutine(EnemyGenerator());
+
 	}
-	
+	IEnumerator EnemyGenerator()
+    {
+        yield return new WaitForSeconds(delay);
+
+        if(active)
+        {
+            var newTransform = transform;
+
+            Instantiate(prefabs[Random.Range(0, prefabs.Length)], newTransform.position, Quaternion.identity);
+        }
+
+        StartCoroutine(EnemyGenerator());
+
+    }
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 }
---
+++ Assets/Code/TileBackground.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileBackground : MonoBehaviour {
+
+    public int textureSize = 32;
+
+	// Use this for initialization
+	void Start ()
+    {
+        float newWidth = Mathf.Ceil(Screen.width / (textureSize * CameraThing.scale));
+        float newHeight = Mathf.Ceil(Screen.height / (textureSize * CameraThing.scale));
+
+        transform.localScale = new Vector3 (newWidth * textureSize, newHeight * textureSize, 1);
+
+        GetComponent<Renderer>().material.mainTextureScale = new Vector3(newWidth, newHeight, 1);
+
+     }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
