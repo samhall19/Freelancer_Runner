7de5db74859acd5ba29c8724e756a5796b7bdf9a
Alex <sandylily66@hotmail.com>
2017-01-20 20:23:25 +0000

    Added first code script

---
+++ Assets/Code/Jump.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+    public float JumpSpeed = 240f;
+    private bool HasJumped = false;
+    private Rigidbody2D body2d;
+ 
+	// Use this for initialization
+	void Awake() {
+
+        body2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            if (HasJumped == false && Input.GetKeyDown("space"))
+            {
+                body2d.velocity = new Vector2(0, JumpSpeed);
+            }
+        }
+	}
+
