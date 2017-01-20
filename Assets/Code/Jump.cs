b9c68af8fe030bede08791580700d47b74b66895
Phil-Sparkes <phil.sparkes95@gmail.com>
2017-01-20 21:18:45 +0000

    Fixed Jump

--- Assets/Code/Jump.cs
+++ Assets/Code/Jump.cs
@@ -1,24 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Jump : MonoBehaviour {
+public class Jump : MonoBehaviour
+{
 
-    public float JumpSpeed = 240f;
+  {
+    public float JumpSpeed = 3f;
     private bool HasJumped = false;
     private Rigidbody2D body2d;
- 
-	// Use this for initialization
-	void Awake() {
+
+    // Use this for initialization
+    void Awake()
+    {
 
         body2d = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-            if (HasJumped == false && Input.GetKeyDown("space"))
-            {
-                body2d.velocity = new Vector2(0, JumpSpeed);
-            }
-        }
-	}
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (HasJumped == false && Input.GetKeyDown("space"))
+        {
+            body2d.velocity = new Vector2(0, JumpSpeed);
+            HasJumped = true;
+        }
+    }
+
+    // When the character collides with an object
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            HasJumped = false;
+        }
+    }
+}
