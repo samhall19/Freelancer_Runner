cd7ab7b21b192b5cc957ac1183e0a1a2bc7548fc
Phil-Sparkes <phil.sparkes95@gmail.com>
2017-01-21 16:50:10 +0000

    Add files via upload

---
+++ Assets/Code/HealthScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour
+{
+    Text TextBox;
+    public float Health = 100;
+    // Use this for initialization
+    void Start()
+    {
+        TextBox = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Health -= (3 * Time.deltaTime);
+        TextBox.text = "health = " + (int)Health;
+    }
+}
---
+++ Assets/Code/Pickup.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+    private Rigidbody2D body2d;
+    // Use this for initialization
+    void Start () {
+        body2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+	void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Item")
+        {
+            GameObject Text = GameObject.Find("Text");
+            HealthScript targetScript = Text.GetComponent<HealthScript>();
+            targetScript.Health += 10;
+            Destroy(other.gameObject);
+        }
+    }
+
+}
+
