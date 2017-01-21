9cd703ae9e75939ea8e929aa13fab77a34240125
Phil-Sparkes <phil.sparkes95@gmail.com>
2017-01-21 14:44:01 +0000

    Add files via upload

---
+++ Assets/Code/textscript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class textscript : MonoBehaviour {
+    private float nextUsage;
+    private float textDelay = 2;//two seconds delay.
+    Text TextBox;
+    string[] FrenchTalk = System.IO.File.ReadAllLines("TestText.txt");
+	// Use this for initialization
+	void Start () {
+        nextUsage = Time.time + textDelay;
+        TextBox = gameObject.GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > nextUsage)
+        {
+            nextUsage = Time.time + textDelay;
+            TextBox.text = FrenchTalk[Random.Range(0, 5)];
+        }
+	}
+}
